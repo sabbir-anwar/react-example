7e18c43851ab011fdad398595b3604b90b28e0e0
sabbir-anwar <protik26@gmail.com>
2020-06-26 17:14:47 +0600

    Navigation

--- src/App.js
+++ src/App.js
@@ -8,20 +8,24 @@ import {
 
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import "./App.css";
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact>
-          <Users />
-        </Route>
-        <Route path="/places/new" exact>
-          <NewPlace />
-        </Route>
-        <Redirect to="/"></Redirect>
-      </Switch>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/places/new" exact>
+            <NewPlace />
+          </Route>
+          <Redirect to="/"></Redirect>
+        </Switch>
+      </main>
     </Router>
   );
 };
